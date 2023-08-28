8b4ae9cee38cb37a74cc1553dda9668f49a289c6
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-28 13:38:03 -0500

    3rd test

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'></meta>
-      </head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   )
