1fee06f3d5a8cf2ed8addb2936540eb980549ce6
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-28 14:01:06 -0500

    mobile test

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
         backgroundImage:`url('https://cdn.motor1.com/images/mgl/ko6VAN/s3/porsche-911-dakar-versatility.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center'
-      }} className='w-screen h-[96vh] lg:h-screen text-white px-4 md:px-8 lg:px-20 xl:px-28 2xl:px-40 pt-12 md:pt-14 lg:pt-32 xl:pt-40 2xl:pt-56 overflow-hidden relative'>
+      }} className='w-screen h-[96vh] lg:h-screen text-white px-4 md:px-8 lg:px-20 xl:px-28 2xl:px-40 pt-12 md:pt-14 lg:pt-32 xl:pt-40 2xl:pt-56 overflow-hidden relative m-0'>
         <div className='leading-3 lg:leading-8'>
             <p>PORSCHE 911</p>
             <motion.h1 
@@ -29,31 +29,18 @@ export default function Hero() {
                 className={`${myFont.className} text-6xl lg:text-[88px]`}
             >DAKAR</motion.h1>            
         </div>
-        <div className='absolute left-4 md:left-8 lg:left-20 xl:left-28 2xl:left-40 bottom-40 lg:bottom-32 xl:bottom-36 font-extrabold text-black md:text-lg leading-6 tracking-tight'>
-            <div className='flex gap-2 items-center'>
-                <BsFillStopwatchFill></BsFillStopwatchFill>
-                <p className='underline'>0 - 100:</p>
-                <p>3,4s</p>
+       <div className='absolute bottom-8 flex flex-col lg:flex-row items-center w-1/2 left-1/2 -translate-x-1/2 lg:-translate-x-0 pl-0 lg:pr-28 lg:justify-between'>
+            <button className='py-2 border border-white lg:-translate-x-1/2 px-12 rounded-xl bg-white/5 backdrop-blur-xl'>Official page</button>
+            <div className='flex gap-4 items-center'>
+                <div className='w-32 lg:w-36 h-[6px] bg-amber-800 cursor-pointer'>
+
+                </div>
+                <div className='w-32 lg:w-36 h-1 bg-slate-400 cursor-pointer'>
+
+                </div>
             </div>
-            <div className='flex gap-2 items-center'>
-                <BsSpeedometer />
-                <p className='underline'>Top speed:</p>
-                <p>245 <span className='text-sm'>km/h</span></p>
-            </div>
-            <div className='flex gap-2 items-center'>
-                <BsSpeedometer />
-                <p className='underline'>Top speed:</p>
-                <p>245 <span className='text-sm'>km/h</span></p>
-            </div>
-            
-        </div>
-        <div className="absolute left-1/2 bottom-20 lg:bottom-12 w-1/2 flex justify-between items-center lg:pr-28 flex-col lg:flex-row gap-4 lg:gap-0">
-            <button className='border border-white text-white font-semibold px-10 lg:px-8 2xl:px-16 bg-white/10 backdrop-blur-xl py-[6px] rounded-xl hover:bg-white hover:border-white hover:text-black transition-colors duration-1000 -translate-x-1/2'>Oficial Page</button>
-            <div className="-translate-x-[25%] lg:-translate-x-0 flex items-center gap-4">
-                <motion.div variants={fadeIn('right', 0)} initial='initial' whileInView={'show'} className='w-36 h-[6px] bg-amber-800 rounded-md cursor-pointer'></motion.div>
-                <motion.div variants={fadeIn('left', 0)} initial='initial' whileInView={'show'} className='w-36 h-1 bg-white/30 rounded-md cursor-pointer'></motion.div>
-            </div>
-        </div>
+       </div>
+        
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from './Components/Hero'
 
 export default function Home() {
   return (
-    <main className='relative' >
+    <main className='relative m-0' >
       <Hero />
     </main>
   )
