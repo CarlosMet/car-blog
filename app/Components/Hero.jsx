3478c58eac02cf05aeb0f3e9dd04cdf0c61ae8b2
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-28 03:33:45 -0500

    framer preview

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
         backgroundImage:`url('https://cdn.motor1.com/images/mgl/ko6VAN/s3/porsche-911-dakar-versatility.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center'
-      }} className='w-screen h-screen text-white px-4 md:px-8 lg:px-20 xl:px-28 2xl:px-40 pt-20 md:pt-14 lg:pt-32 xl:pt-40 2xl:pt-56 overflow-hidden relative'>
+      }} className='w-screen h-screen text-white px-4 md:px-8 lg:px-20 xl:px-28 2xl:px-40 pt-12 md:pt-14 lg:pt-32 xl:pt-40 2xl:pt-56 overflow-hidden relative'>
         <div className='leading-3 lg:leading-8'>
             <p>PORSCHE 911</p>
             <motion.h1 
@@ -29,7 +29,7 @@ export default function Hero() {
                 className={`${myFont.className} text-6xl lg:text-[88px]`}
             >DAKAR</motion.h1>            
         </div>
-        <div className='absolute left-4 md:left-8 lg:left-20 xl:left-28 2xl:left-40 bottom-36 lg:bottom-32 xl:bottom-36 font-extrabold text-black md:text-lg leading-6 tracking-tight'>
+        <div className='absolute left-4 md:left-8 lg:left-20 xl:left-28 2xl:left-40 bottom-40 lg:bottom-32 xl:bottom-36 font-extrabold text-black md:text-lg leading-6 tracking-tight'>
             <div className='flex gap-2 items-center'>
                 <BsFillStopwatchFill></BsFillStopwatchFill>
                 <p className='underline'>0 - 100:</p>
@@ -47,11 +47,11 @@ export default function Hero() {
             </div>
             
         </div>
-        <div className="absolute left-1/2 bottom-12 w-1/2 flex justify-between items-center lg:pr-28 flex-col lg:flex-row gap-4 lg:gap-0">
+        <div className="absolute left-1/2 bottom-20 lg:bottom-12 w-1/2 flex justify-between items-center lg:pr-28 flex-col lg:flex-row gap-4 lg:gap-0">
             <button className='border border-white text-white font-semibold px-10 lg:px-8 2xl:px-16 bg-white/10 backdrop-blur-xl py-[6px] rounded-xl hover:bg-white hover:border-white hover:text-black transition-colors duration-1000 -translate-x-1/2'>Oficial Page</button>
             <div className="-translate-x-[25%] lg:-translate-x-0 flex items-center gap-4">
-                <div className='w-36 h-[6px] bg-amber-800 rounded-md cursor-pointer'></div>
-                <div className='w-36 h-1 bg-white/30 rounded-md cursor-pointer'></div>
+                <motion.div variants={fadeIn('right', 0)} initial='initial' whileInView={'show'} className='w-36 h-[6px] bg-amber-800 rounded-md cursor-pointer'></motion.div>
+                <motion.div variants={fadeIn('left', 0)} initial='initial' whileInView={'show'} className='w-36 h-1 bg-white/30 rounded-md cursor-pointer'></motion.div>
             </div>
         </div>
     </div>
